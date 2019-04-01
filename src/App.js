f9effc5b5ca15a5b18a0dd4f69bf0b854da75ce5
vezeveer <mortalmanny@live.com>
2019-04-01 11:11:13 +0800

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import Home from './components/home'
+import About from './components/about'
+import Projects from './components/projects'
+import Photoshop from './components/photoshop'
+import Contact from './components/contact'
+import './css/App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="bg">
+          <img className="left" src="/imgs/left-part.jpg" alt="" />
+          <img className="right" src="/imgs/right-part.jpg" alt="" />
+        </div>
+        <Router>
+          <div className="container">
+            <div className="menu-container">
+              <div className="menu-position">
+                <Link to="/"><div className="menu">Home</div></Link>
+                <Link to="/about"><div className="menu">About</div></Link>
+                <div className="menu">Projects</div>
+                <div className="menu">Photoshop</div>
+                <div className="menu">Contact</div>
+              </div>
+            </div>
+            <div className="display-container">
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+                <Route exact path="/projects" component={Projects} />
+                <Route exact path="/photoshop" component={Photoshop} />
+                <Route exact path="/contact" component={Contact} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
       </div>
     );
   }
---
+++ src/components/projects/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div className="projects-container">
+      <div>JavaScript</div>
+      <div>Web</div>
+      <div>Games</div>
+      <div>simple Web</div>
+    </div>
+  )
+}
