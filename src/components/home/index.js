dcab86b2d89bf38515c898efa6c6cbea04b16e86
vezeveer <mortalmanny@live.com>
2019-04-01 22:01:00 +0800

    added home

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,9 +1,21 @@
-import React from 'react';
 
-export default () => {
+import React from 'react'
+import './home.css'
+
+const Home = () => {
   return (
-    <div>
-      Home here
+    <div className="home-position">
+      <header className="home-container">
+        <div className="logo-lvl-1">
+          <div className="logo-lvl-2">
+            <h4>Hi, I'm</h4>
+            <img className="ep-logo" src="./imgs/ep_logo.svg" alt="logo" />
+            <h4>And I am A Web Developer</h4>
+          </div>
+        </div>
+      </header>
     </div>
   )
 }
+
+export default Home
